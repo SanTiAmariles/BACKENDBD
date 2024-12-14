177bd283c20c349a36bb01c6e5c0b98a33e63c40
santi <erik.amariles@correounivalle.edu.co>
2024-12-13 20:21:42 -0500

    Backend con conexióna los registros

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,15 @@
+# api/serializers.py
+
 from rest_framework import serializers
 from .models import Estudiante, Profesor
 
+# Serializador para Estudiantes
 class EstudianteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estudiante
-        fields = ['nombres', 'apellidos', 'correoInstitucional', 'numeroCelular',  'password', 'codigoEstudiante','codigoEstudiante']
+        fields = ['nombres', 'apellidos', 'correoInstitucional', 'numeroCelular', 'codigoEstudiante']
 
-
+# Serializador para Profesores
 class ProfesorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profesor
